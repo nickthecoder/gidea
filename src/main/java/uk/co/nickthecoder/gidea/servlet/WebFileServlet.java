6a4984349f1471fcf1cb5004aad6f2fa040ef6d7
Nick Robinson <nickthecoder@gmail.com>
2015-12-15 09:44:56 +0000

    Bypass tomcat bug for request.getPathInfo()

--- src/main/java/uk/co/nickthecoder/gidea/servlet/WebFileServlet.java
+++ src/main/java/uk/co/nickthecoder/gidea/servlet/WebFileServlet.java
@@ -21,9 +21,41 @@ public abstract class WebFileServlet extends GideaServlet
         super();
     }
 
+    /**
+     * When using tomcat 7.0.28, request.getPathInfo was returning the wrong thing.
+     * If there was an escaped semi-colon, then it was treating it as unescaped, and terminating the path info at that point.
+     * I think it was also doing the wrong thing for utf-8 characters that were % encoded.
+     * Jetty did the right thing, making test and production behave differently.
+     * 
+     * This is used in place of request.getPathInfo() to get the result I expect.
+     */
+    public static String getPathInfo(HttpServletRequest request)
+    {
+        String uri = request.getRequestURI();
+        String pre = request.getContextPath() + request.getServletPath();
+
+        if (uri.startsWith(pre)) {
+            // Remove the context path, and the servlet path
+            String path = uri.substring(pre.length());
+            // Remove parameters (such as jsessionid), which use semi-colon
+            // Note that regular parameters, using ?foo=bar format are NOT in
+            // uri, so no need to remove them.
+            path = path.replaceAll(";.*", "");
+            try {
+                return URLDecoder.decode(path, "UTF-8");
+            } catch (Exception e) {
+                // Do nothing. Fall through to default request.getPathInfo()
+            }
+        }
+        // Hmm, didn't expect that use the normal version of getPathInfo as a
+        // fall back.
+        return request.getPathInfo();
+
+    }
+
     public String getPath(HttpServletRequest request) throws ServletException
     {
-        String path = request.getPathInfo();
+        String path = getPathInfo(request);
 
         if ((path == null) || (path.length() < 2)) {
 
